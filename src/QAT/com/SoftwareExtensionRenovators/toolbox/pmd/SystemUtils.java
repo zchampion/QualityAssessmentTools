63752314900cb4181a5fb880285530c31b23233c
applejaxx <applejaxx@users.noreply.github.com>
2018-11-28 19:27:57 -0700

    Update Javadocs

--- src/QAT/com/SoftwareExtensionRenovators/toolbox/pmd/SystemUtils.java
+++ src/QAT/com/SoftwareExtensionRenovators/toolbox/pmd/SystemUtils.java
@@ -1,8 +1,18 @@
 package com.SoftwareExtensionRenovators.toolbox.pmd;
 
 import java.util.Locale;
+/**
+ * Checks which OS is being used by the user.
+ * @author Ted Mittelstaedt, from an original by Tom Copeland
+ * @author Egor Muscat, Jackie Nugent, Mark Huntington, Zac Champion
+ * @version 0.6.1
+ */
 
 public class SystemUtils{
+    /**
+     * Checks to see if user's OS is Windows
+     * @return boolean
+     */
     public static boolean isWindows(){
         String osname = System.getProperty("os.name");
         return osname.toLowerCase(Locale.ROOT).contains("win");
