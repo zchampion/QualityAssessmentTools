a287a2335f0e5cd5af52602dd2855f4a869af712
applejaxx <applejaxx@users.noreply.github.com>
2018-12-01 18:25:54 -0700

    Update Javadocs

--- src/QAT/com/SoftwareExtensionRenovators/bluejmanager/BlueJPropertiesAdapter.java
+++ src/QAT/com/SoftwareExtensionRenovators/bluejmanager/BlueJPropertiesAdapter.java
@@ -1,22 +1,4 @@
-////////////////////////////////////////////////////////////////////////////////
-// BlueJ Checkstyle extension:
-//    Checks Java source code for adherence to a set of rules.
-// Copyright (C) 2007  Stephen Edwards
-//
-// This library is free software; you can redistribute it and/or
-// modify it under the terms of the GNU Lesser General Public
-// License as published by the Free Software Foundation; either
-// version 2.1 of the License, or (at your option) any later version.
-//
-// This library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public
-// License along with this library; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-////////////////////////////////////////////////////////////////////////////////
+
 package com.SoftwareExtensionRenovators.bluejmanager;
 
 import java.util.*;
@@ -29,7 +11,8 @@ import bluej.extensions.BlueJ;
  * extension proxy object to access BlueJ's property settings for that
  * extension.
  * @author Stephen Edwards
- * @version $Id: BlueJPropertiesAdapter.java,v 1.1 2007/08/19 03:13:03 stedwar2 Exp $
+ * @author Egor Muscat, Jackie Nugent, Mark Huntington, Zac Champion
+ * @version 0.6.1
  */
 public class BlueJPropertiesAdapter
     extends Properties
