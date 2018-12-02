71a53381a16153aa8a452d6e905d519a18c194ca
applejaxx <applejaxx@users.noreply.github.com>
2018-12-01 18:26:40 -0700

    Updated Javadocs

--- src/QAT/com/SoftwareExtensionRenovators/bluejmanager/ExtensionMenu.java
+++ src/QAT/com/SoftwareExtensionRenovators/bluejmanager/ExtensionMenu.java
@@ -1,22 +1,4 @@
-////////////////////////////////////////////////////////////////////////////////
-// BlueJ Checkstyle extension:
-//    Checks Java source code for adherence to a set of rules.
-// Copyright (C) 2003-2004  Rick Giles
-//
-// This library is free software; you can redistribute it and/or
-// modify it under the terms of the GNU Lesser General Public
-// License as published by the Free Software Foundation; either
-// version 2.1 of the License, or (at your option) any later version.
-//
-// This library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public
-// License along with this library; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-////////////////////////////////////////////////////////////////////////////////
+
 package com.SoftwareExtensionRenovators.bluejmanager;
 
 import com.SoftwareExtensionRenovators.toolbox.pmd.PMDAction;
@@ -31,8 +13,9 @@ import bluej.extensions.*;
 
 /**
  * Manages the Quality Assessment Tools extension menu items
- * @author Rick Giles
- * @version $Id: ExtensionMenu.java,v 1.5 2007/08/19 03:13:53 stedwar2 Exp $
+ * @author Rick Giles 
+ * @author Egor Muscat, Jackie Nugent, Mark Huntington, Zac Champion
+ * @version 0.6.1
  */
 public class ExtensionMenu extends MenuGenerator {
     /**
